04dd83df2c6575fe05a6250af9bd78869291662b
nickstuffz <112445360+nickstuffz@users.noreply.github.com>
2023-10-10 01:02:22 -0700

    Add placeholders for form

--- vite-project/src/components/Sidebar.jsx
+++ vite-project/src/components/Sidebar.jsx
@@ -1,17 +1,33 @@
-function Input({ id, type, handleChange }) {
+function Input({ id, type, placeholder, handleChange }) {
   if (id === "description") {
-    return <textarea id={id} onChange={(event) => handleChange(event, id)} />;
+    return (
+      <textarea
+        id={id}
+        placeholder={placeholder}
+        onChange={(event) => handleChange(event, id)}
+      />
+    );
   }
   return (
-    <input id={id} type={type} onChange={(event) => handleChange(event, id)} />
+    <input
+      id={id}
+      type={type}
+      placeholder={placeholder}
+      onChange={(event) => handleChange(event, id)}
+    />
   );
 }
 
-function InputPair({ name, id, type, handleChange }) {
+function InputPair({ name, id, type, placeholder, handleChange }) {
   return (
     <div className="flex flex-col">
       <label htmlFor={id}>{name}</label>
-      <Input id={id} type={type} handleChange={handleChange} />
+      <Input
+        id={id}
+        type={type}
+        placeholder={placeholder}
+        handleChange={handleChange}
+      />
     </div>
   );
 }
@@ -32,24 +48,28 @@ function Sidebar({ handleChange }) {
           name="Full Name"
           id="full_name"
           type="text"
+          placeholder="Mister Potato"
           handleChange={handleChange}
         />
         <InputPair
           name="Email"
           id="email"
           type="email"
+          placeholder="potatopower@gmail.com"
           handleChange={handleChange}
         />
         <InputPair
           name="Address"
           id="address"
           type="text"
+          placeholder="1234 Starchy Ave"
           handleChange={handleChange}
         />
         <InputPair
           name="Phone Number"
           id="phone_number"
           type="tel"
+          placeholder="123-123-1234"
           handleChange={handleChange}
         />
       </Card>
@@ -60,12 +80,14 @@ function Sidebar({ handleChange }) {
           name="School"
           id="school"
           type="text"
+          placeholder="University of Vegetable"
           handleChange={handleChange}
         />
         <InputPair
           name="Program"
           id="program"
           type="text"
+          placeholder="Spud History"
           handleChange={handleChange}
         />
         <InputPair
@@ -88,12 +110,14 @@ function Sidebar({ handleChange }) {
           name="Organization"
           id="organization"
           type="text"
+          placeholder="Carbohydrate Corporation"
           handleChange={handleChange}
         />
         <InputPair
           name="Position"
           id="position"
           type="text"
+          placeholder="Research Intern"
           handleChange={handleChange}
         />
         <InputPair
@@ -111,6 +135,7 @@ function Sidebar({ handleChange }) {
         <InputPair
           name="Description"
           id="description"
+          placeholder="Researched potato facts."
           handleChange={handleChange}
         />
       </Card>
